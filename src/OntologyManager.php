18127733893c1d3effd66dd04e1640e12c107fcd
Tomoki Aburatani <aburatanitomoki@gmail.com>
2022-10-18 23:25:19 +0900

    adjust cors request

--- src/OntologyManager.php
+++ src/OntologyManager.php
@@ -65,6 +65,7 @@ class OntologyManager {
 
     public function showJson($data) {
 	// JSONデータを表示するための関数
+	header("Access-Control-Allow-Origin: *");
         header("Content-Type: application/json; charset=utf-8");
 	echo json_encode($data);
 	return;
